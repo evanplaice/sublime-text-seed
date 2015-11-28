2a95ce6da75582a74d35646fd11372fef7219819
Evan Plaice <evanplaice@gmail.com>
2015-11-27 18:44:17 -0800

    Detection for application install
    The `isInstalled()` function takes a application path and checks to see
    whether it is installed.

--- setup.py
+++ setup.py
@@ -1,24 +1,39 @@
 import platform
 import sys
+import subprocess
 
 platform = platform.system()
+app_path = ''
 
 def main():
   if platform == 'Darwin':
     print('OSX detected')
+    is_installed(app_path);
     sys.exit(0)
 
   if platform == 'Linux':
     print('Linux detected...')
+    is_installed(app_path);
     sys.exit(0)
 
   if platform == 'Windows':
     print('Windows detected...')
+    is_installed(app_path);
     sys.exit(0)
 
   else:
     raise Exception('Operating system not supported');
     sys.exit(1)
+
+def is_installed(app_path):
+  try:
+    subprocess.call([app_path]);
+    print('Sublime is installed...')
+    return True
+
+  except OSError as e:
+    print('Sublime not installed...')
+    return False
 
 if __name__ == '__main__':
   main()
