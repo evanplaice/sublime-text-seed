d8b084155934d79d3e0f98a5ca9db6a4a880ff27
Evan Plaice <evanplaice@gmail.com>
2015-11-27 19:06:59 -0800

    Application paths added

--- setup.py
+++ setup.py
@@ -8,16 +8,21 @@ app_path = ''
 def main():
   if platform == 'Darwin':
     print('OSX detected')
+    # TODO: test this in OSX
+    app_path = '/Applications/Sublime Text.app'
     is_installed(app_path);
     sys.exit(0)
 
   if platform == 'Linux':
     print('Linux detected...')
+    app_path = 'sublime-text'
     is_installed(app_path);
     sys.exit(0)
 
   if platform == 'Windows':
     print('Windows detected...')
+    # TODO: do a windows instal to determine the default install location
+    # app_path = ''
     is_installed(app_path);
     sys.exit(0)
 
