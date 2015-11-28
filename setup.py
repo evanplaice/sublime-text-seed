aa7570edca8e137a72c96e3de2c4af3c4251e5e1
Evan Plaice <evanplaice@gmail.com>
2015-11-27 18:23:19 -0800

    Mock function definition for isInstalled() added

--- setup.py
+++ setup.py
@@ -18,3 +18,15 @@ if platform == 'Windows':
   print('Windows detected...')
 
   sys.exit(0)
+
+def isInstalled(platform):
+
+  if platform == 'Darwin':
+    return
+
+  if platform == 'Linux':
+    return
+
+  if platform == 'Windows':
+    return
+
